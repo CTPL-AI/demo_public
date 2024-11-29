95e0016aab808735514691b7cd9eac184eac273a
upendraCTPL <upendra.j@ctpl.io>
2024-11-29 10:11:44 +0000

    appsepc.yml file corrected

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -12,7 +12,13 @@ curl -sSL https://install.python-poetry.org | python3 -
 # Add Poetry to PATH
 export PATH="$HOME/.local.bin:$PATH"
 
-#Install dependencies using poetry
-cd /home/ubuntu/demo_public
-poetry install
+# Verify Poetry installation
+poetry --version || { echo "Poetry installation failed!"; exit 1; }
 
+# Navigate to the application directory
+echo "Navigating to application directory..."
+cd /home/ubuntu/demo_public || { echo "Directory not found!"; exit 1; }
+
+# Install dependencies using Poetry
+echo "Installing dependencies with Poetry..."
+poetry install --no-dev  # Include --no-dev to install only production dependencies
